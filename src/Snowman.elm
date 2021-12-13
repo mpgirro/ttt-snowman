59fe9cf65e1805099817aac62bffd628a958fe50
Maximilian Irro <max@irro.at>
2021-12-14 00:08:34 +0100

    Visualize snowman

--- src/Snowman.elm
+++ src/Snowman.elm
@@ -1,10 +1,11 @@
 module Snowman exposing (..)
 
 import Browser exposing (Document)
-import Html exposing (Html, button, div, input, p, text)
+import Browser.Events exposing (onKeyPress)
+import Html exposing (Html, button, code, div, h1, input, p, pre, span, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
-import String exposing (fromList, uncons)
+import String exposing (fromChar, fromList, uncons)
     
 
 ---- PROGRAM ----
@@ -26,7 +27,7 @@ type alias Model =
     { snowman : Snowman
     , secretWord : String
     , letter : List Char
-    , errorsLeft : Int
+    , errors : Int
     }
 
 type Snowman 
@@ -37,7 +38,7 @@ initModel =
     { snowman = EmptySnowman 
     , secretWord = randomWord
     , letter = []
-    , errorsLeft = 6
+    , errors = 6
     }
 
 init : flags -> ( Model, Cmd Msg )
@@ -77,11 +78,8 @@ subscriptions _ =
 
 updateInput : Model -> String -> Msg
 updateInput model query =
-    let
-        bar = uncons query
-        foo = typedChar bar
-    in 
-        UpdateSnowman foo
+    UpdateSnowman (typedChar (uncons query))
+        
 
 typedChar : Maybe ( Char, String ) -> Char 
 typedChar maybe =
@@ -104,17 +102,184 @@ view model =
 viewContent : Model -> Html Msg
 viewContent model =
     div []
-        [ div [] [ p [] [ text model.secretWord ] ]
+        [ viewHeader
+        , viewQuiz model
         , input [ placeholder "Guess the word", value "", onInput (updateInput model) ] []
-        , div [] [ text (fromList model.letter) ]
+        , viewHistory model
+        , viewSnowman model
+        , viewFooter
         ]
 
 
 --viewContent : Model -> ( String, Html Msg )
 --viewContent model = ( "TTT Snowman", foo)
-    
+
+viewHeader : Html msg
+viewHeader = h1 [] [ text "TTT Snowman" ] 
+
+viewQuiz : Model -> Html msg
+viewQuiz model = 
+    div [] 
+        [ p [] [ span [] [ text "Quiz" ], text model.secretWord ] ]
+
+viewHistory : Model -> Html Msg
+viewHistory model = 
+    p [] 
+    [ text "Last typed letters:", viewLetters model ]
+
+
+viewLetters : Model -> Html Msg
+viewLetters model =
+    let
+        toSpan : Char -> Html Msg
+        toSpan c = span [] [ text (fromChar c) ]
+
+        toSpanList : List Char -> List (Html Msg)
+        toSpanList cs =
+            List.map toSpan cs
+    in
+        div [] (toSpanList model.letter)
+
+viewFooter : Html Msg
+viewFooter = 
+    div [] [ text "Made with ❤️ and Elm for viesure by Max" ]
 
 ---- HELPER ----
 
 randomWord : String
 randomWord = "ERNSTL"
+
+
+viewSnowman : Model -> Html msg
+viewSnowman model =
+    let
+        snowman = 
+            case model.errors of
+                1 -> 
+                    [ text snowmanText1
+                    , text snowmanEmptyText5
+                    , text snowmanEmptyText5
+                    , text snowmanEmptyText5
+                    , text snowmanEmptyText5
+                    , text snowmanEmptyText4
+                    ]
+                2 ->
+                    [ text snowmanText1
+                    , text snowmanText2
+                    , text snowmanEmptyText5
+                    , text snowmanEmptyText5
+                    , text snowmanEmptyText5
+                    , text snowmanEmptyText4
+                    ]
+                3 ->
+                    [ text snowmanText1
+                    , text snowmanText2
+                    , text snowmanText3
+                    , text snowmanEmptyText5
+                    , text snowmanEmptyText5
+                    , text snowmanEmptyText4
+                    ]
+                4 ->
+                    [ text snowmanText1
+                    , text snowmanText2
+                    , text snowmanText3
+                    , text snowmanText4
+                    , text snowmanEmptyText5
+                    , text snowmanEmptyText4
+                    ]
+                5 ->
+                    [ text snowmanText1
+                    , text snowmanText2
+                    , text snowmanText3
+                    , text snowmanText4
+                    , text snowmanText5
+                    , text snowmanEmptyText4
+                    ]
+                6 ->
+                    [ text snowmanText1
+                    , text snowmanText2
+                    , text snowmanText3
+                    , text snowmanText4
+                    , text snowmanText5
+                    , text snowmanText6
+                    ]
+                _ -> [ text "ERROR" ]
+        in 
+            pre [] snowman
+
+snowmanEmptyText4 : String
+snowmanEmptyText4 =
+    """
+
+
+
+
+    """
+
+snowmanEmptyText5 : String
+snowmanEmptyText5 =
+    """
+
+
+
+
+
+    """
+
+snowmanText1 : String
+snowmanText1 = 
+    """
+                       .--------.
+  *               .    |________|        .          *
+                       |      __|/\\
+            *        .-'======\\_\\o/. 
+    """
+
+snowmanText2 : String
+snowmanText2 = 
+    """
+                    /___________<>__\\
+              ||||||  /  (o) (o)  \\
+              |||||| |   _  O  _   |          .
+    .         |||||| |  (_)   (_)  |
+              ||||||  \\   '---'   /    * 
+    """
+
+snowmanText3 : String
+snowmanText3 = 
+    """
+              \\====/   [~~~~~~~~~]
+               \\//  _/~||~`|~~~~~\\_
+               _||-'`/  ||  |      \\`'-._       *
+       *    .-` )|  ;   ||  |)      ;    '.
+           /    `--.|   ||  |       |      `\\ 
+    """
+
+snowmanText4 : String
+snowmanText4 = 
+    """
+          |         \\   |||||)      |-,      \\         .
+           \\       .;       _       ; |_,    |
+            `'''||` ,\\     (_)     /,    `.__/
+                ||.`  '.         .'  `.             *
+     *          ||       ` ' ' `       \\
+    """
+
+snowmanText5 : String
+snowmanText5 = 
+    """
+                ||                      ;
+  .          *  ||                      |    .
+                ||                      |              *
+                ||                      |
+.__.-""-.__.-\"""||                      ;.-""\"-.__.-""-.__. 
+    """
+
+snowmanText6 : String
+snowmanText6 = 
+    """
+                ||                     /
+           jgs  ||'.                 .'
+                ||  '-._  _ _  _ _.-'
+               `""` 
+    """
